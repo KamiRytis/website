3048a5363cadd90465493c4633d030d62569690c
KamiRytis <rytiskaminskas0@gmail.com>
2023-02-02 13:31:38 +0200

    Header finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,30 @@
+import {Link, Outlet} from 'react-router-dom';
+import UserContext from '../contexts/UserContext';
+import { useContext } from 'react';
+
+
 const Header = () => {
-    return ( <></> );
+    const {loggedInUser}=useContext(UserContext);
+
+    return ( 
+    <>
+        {
+            loggedInUser ?
+            <div className='menu'>
+                <img src="" alt="logo" />
+                <Link to='/'>Home</Link>
+                <Link to='/add'>Add new Post</Link>
+             </div>
+            :
+            <div className='menu'>
+                <img src="" alt="logo" />
+                <Link to='/login'>Log in</Link>
+                <Link to='/register'>Register</Link>
+            </div>
+        }    
+        <Outlet />
+    </> 
+    );
 }
  
 export default Header;
