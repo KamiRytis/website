f4b62bd868067cc9cf363b3128fe7277086468ac
KamiRytis <rytiskaminskas0@gmail.com>
2023-02-02 13:07:48 +0200

    Context files created and get fuction set

--- src/contexts/PostContext.jsx
+++ src/contexts/PostContext.jsx
@@ -1,12 +1,29 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { createContext } from "react";
 
 const PostContext = createContext();
 
 const PostProvider = ({children}) => {
+    const [posts,setPosts]=useState([])
+
+    const getPosts = async () => {
+        const fetchinamiPostai =await fetch("http://localhost:3000/posts")
+        .then(res=>res.json());
+        setPosts(fetchinamiPostai);
+    }
+    useEffect(()=>{
+        getPosts()
+    },[])
+
+
+
     return ( 
     <>
         <PostContext.Provider
-            value={{}}
+            value={{
+                posts
+            }}
         >
             {children}
         </PostContext.Provider>
--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,12 +1,28 @@
+import { useState, useEffect } from "react";
 import { createContext } from "react";
 
 const UserContext = createContext();
 
 const UserProvider = ({children}) => {
+    const [users,setUsers] =useState([])
+    const [loggedInUser, setLoggedInUser] =useState();
+
+    const getUsers = async () => {
+        const usersFetched = await fetch("http://localhost:3000/users").then(res=>res.json());
+        setUsers(usersFetched);
+    }
+    useEffect(()=>{
+        getUsers()
+    },[])
+
     return ( 
     <>
         <UserContext.Provider
-            value={{}}
+            value={{
+                users,
+                loggedInUser,
+                setLoggedInUser
+            }}
         >
             {children}
         </UserContext.Provider>
